9d21ece8cfa6e5975bbad86f9b0fbffaf61dcf07
Iader E. Garcia Gomez <iadergg@gmail.com>
2022-11-23 15:06:56 -0500

    first commit

---
+++ src/TodoCounter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const styles = {};
+
+function TodoCounter() {
+    return (
+        <h2 style={styles}>Has completado 2 de 3 TODOs</h2>
+    );
+}
+
+export { TodoCounter };
