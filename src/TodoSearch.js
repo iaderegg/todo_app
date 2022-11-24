0f957f087ebbce7c0e11e1fda42c752cefc0f690
Iader E. Garcia Gomez <iadergg@gmail.com>
2022-11-24 15:52:32 -0500

    searchValue state

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './TodoSearch.css';
 
-function TodoSearch() {
+function TodoSearch({searchValue, setSearchValue}) {
 
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        setSearchValue(event.target.value);
     };
 
     return (
         <input placeholder="Some task"
                className="TodoSearch"
+               value={searchValue}
                onChange={onSearchValueChange}
         />
     );
